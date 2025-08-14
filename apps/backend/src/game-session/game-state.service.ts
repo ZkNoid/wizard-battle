4e29baabe22c0ef657ac338f411162d34c9db7bd
Alexandr Shcherbatyuk <a.shcherbatyuk@gmail.com>
2025-08-14 16:40:18 +0300

    fix: Server deploy

--- apps/backend/src/game-session/game-state.service.ts
+++ apps/backend/src/game-session/game-state.service.ts
@@ -26,7 +26,7 @@ interface SocketMapping {
 
 @Injectable()
 export class GameStateService {
-    private redisClient = createClient({ url: process.env.REDIS_URL || 'redis://localhost:6379' });
+    private redisClient = createClient({ url: process.env.REDIS_URL});
     private instanceId = `${process.pid}-${Date.now()}`;
 
     constructor() {
