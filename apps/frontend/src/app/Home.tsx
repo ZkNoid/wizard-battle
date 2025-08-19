4babdcb35adb0aab8350af82700074109e90c672
aii23 <a.ivlev.om@gmail.com>
2025-08-19 21:45:55 +0300

    Move to dynamic rendering

---
+++ apps/frontend/src/app/Home.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import HomePage from "@/components/HomePage";
+import { useUserInformationStore } from "@/lib/store/userInformationStore";
+import { useEffect } from "react";
+import { io } from "socket.io-client";
+import { Stater } from "../../../common/stater/stater";
+import { Field } from "o1js";
+
+export default function Home() {
+  const { socket, setSocket, setStater } = useUserInformationStore();
+  useEffect(() => {
+    console.log("useEffect");
+    let socket = io(process.env.NEXT_PUBLIC_API_URL!);
+    setSocket(socket);
+    console.log(socket);
+
+    const stater = Stater.default();
+    console.log(stater);
+    setStater(stater);
+
+    socket.on("connect", () => {
+      console.log("connected");
+      console.log(Field.from(10).toString());
+    });
+  }, []);
+
+  return <HomePage />;
+}
