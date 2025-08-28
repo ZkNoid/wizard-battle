e08fd805a124b124d3273617ebed9b3a6bf97334
Neo-Gar <nvatlecov@mail.ru>
2025-08-28 12:48:05 +0200

    Added tiles description

--- apps/frontend/src/components/MapEditor/Tile.tsx
+++ apps/frontend/src/components/MapEditor/Tile.tsx
@@ -1,7 +1,7 @@
-"use client";
+'use client';
 
-import { TileBorder } from "./assets/tile-border";
-import Image from "next/image";
+import { TileBorder } from './assets/tile-border';
+import Image from 'next/image';
 
 export function Tile({
   image,
@@ -15,23 +15,28 @@ export function Tile({
   onClick: () => void;
 }) {
   return (
-    <button onClick={onClick} className="flex flex-row items-center gap-4">
-      <div className="flex flex-col gap-1">
-        <span className="font-pixel text-main-gray text-lg font-bold">
-          {title}
-        </span>
-        <div className="relative size-20 overflow-hidden">
-          <Image
-            src={image}
-            alt={title}
-            width={48}
-            height={48}
-            className="size-18 absolute inset-1"
-          />
-          <TileBorder className="absolute inset-0 -z-[1] h-full w-full" />
+    <button onClick={onClick} className="max-w-100 flex flex-col gap-1">
+      <span className="font-pixel text-main-gray mr-auto text-lg font-bold">
+        {title}
+      </span>
+      <div className="flex flex-row items-center gap-4">
+        <div className="flex flex-col gap-1">
+          <div className="relative size-20 overflow-hidden">
+            <Image
+              src={image}
+              alt={title}
+              width={48}
+              height={48}
+              className="size-18 absolute inset-1"
+            />
+            <TileBorder className="absolute inset-0 -z-[1] h-full w-full" />
+          </div>
+          {/* Here be tiles count */}
         </div>
+        <span className="font-pixel text-main-gray text-left text-xs">
+          {description}
+        </span>
       </div>
-      <span className="font-pixel text-main-gray text-sm">{description}</span>
     </button>
   );
 }
