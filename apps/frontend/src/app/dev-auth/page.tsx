b6060e5da78dbe8d4fcf5edc87169eba6aca3419
Neo-Gar <nvatlecov@mail.ru>
2025-09-07 14:05:16 +0200

    Fixed dependency & moved to node 22

--- apps/frontend/src/app/dev-auth/page.tsx
+++ apps/frontend/src/app/dev-auth/page.tsx
@@ -1,4 +1,14 @@
 import { DevAuthPage } from '@repo/dev-auth';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Protected Page',
+  description: 'You probably arrived here by accident',
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
 
 export default function DevLoginPage() {
   return <DevAuthPage />;
