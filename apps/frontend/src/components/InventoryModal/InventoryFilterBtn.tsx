a69897a1dbb0993d00c62941d160e56ab295ff3e
Ivan Kireev <31880548+sm4chnik@users.noreply.github.com>
2025-12-22 20:41:15 -0300

    rm SmallBtn

--- apps/frontend/src/components/InventoryModal/InventoryFilterBtn.tsx
+++ apps/frontend/src/components/InventoryModal/InventoryFilterBtn.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 import { Button } from '../shared/Button';
-import { SmBtn } from './assets/sm-btn';
 
 export interface IInventoryFilterBtnProps {
   isActiveFilter: boolean;
@@ -23,7 +22,7 @@ export default function InventoryFilterBtn({
       className={'col-span-2 flex h-16 w-full flex-row items-center gap-2.5'}
       onClick={() => handleChangeFilter()}
     >
-      {imgSrc ? (
+      {imgSrc && (
         <Image
           src={imgSrc}
           width={32}
@@ -31,8 +30,6 @@ export default function InventoryFilterBtn({
           alt={alt ? alt : title}
           className="h-7 w-8 object-contain object-center"
         />
-      ) : (
-        <SmBtn className="-z-1 absolute inset-0 size-full" />
       )}
       <span className="font-pixel text-main-gray text-lg font-bold">
         {title}
