c8bab77b33bdaca53d535c0c5cb64a93c5c6fe2a
Ivan Kireev <31880548+sm4chnik@users.noreply.github.com>
2026-01-20 19:15:14 -0300

    fix modal size

--- apps/frontend/src/components/shared/ConfirmModal/index.tsx
+++ apps/frontend/src/components/shared/ConfirmModal/index.tsx
@@ -14,19 +14,19 @@ interface ConfirmModalProps {
 export default function ConfirmModal({ title, description, onConfirm, onCancel, confirmButtonText = 'Confirm', cancelButtonText = 'Cancel' }: ConfirmModalProps) {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 font-pixel text-main-gray">
-      <div className="relative w-[277px] h-[231px]" onClick={(e) => e.stopPropagation()}>
+      <div className="relative w-70 h-70" onClick={(e) => e.stopPropagation()}>
         <div className="relative z-10 flex h-full w-full flex-col items-center justify-between gap-2">
-          <h1 className="text-md text-main-gray text-center leading-relaxed mt-4">
+          <h1 className="text-xl text-main-gray text-center leading-relaxed mt-4">
             {title}
           </h1>
           <p className="text-xs text-main-gray text-center leading-relaxed px-4 font-thin">
             {description}
           </p>
           <div className="flex w-full gap-1 mb-4 px-3">
-            <Button variant="red" onClick={onCancel} className="flex-1 h-12 px-2">
+            <Button variant="red" onClick={onCancel} className="flex-1 h-12 px-2 text-sm">
               {cancelButtonText}
             </Button>
-            <Button variant="green" onClick={onConfirm} className="flex-1 h-12 px-2">
+            <Button variant="green" onClick={onConfirm} className="flex-1 h-12 px-2 text-sm">
               {confirmButtonText}
             </Button>
           </div>
