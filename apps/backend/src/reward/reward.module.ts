761b383846838159537ce5ad1a9b56da00f729e1
Alexandr Shcherbatyuk <a.shcherbatyuk@gmail.com>
2026-02-12 20:11:08 +0300

    fix build

--- apps/backend/src/reward/reward.module.ts
+++ apps/backend/src/reward/reward.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { RewardController } from './reward.controller';
 import { RewardService } from './reward.service';
 import { UserInventoryModule } from '../user-inventory/user-inventory.module';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [UserInventoryModule],
+  imports: [UserInventoryModule, UserModule],
   controllers: [RewardController],
   providers: [RewardService],
   exports: [RewardService],
