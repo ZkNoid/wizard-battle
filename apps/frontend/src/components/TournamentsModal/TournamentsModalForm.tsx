4c666d43ec54335ab1ea932dffbe7f5751f1485d
Ivan Kireev <31880548+sm4chnik@users.noreply.github.com>
2026-03-09 16:50:14 -0300

    fix bg and size of form

--- apps/frontend/src/components/TournamentsModal/TournamentsModalForm.tsx
+++ apps/frontend/src/components/TournamentsModal/TournamentsModalForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { MarketModalBg } from '../MarketModal/assets/market-modal-bg';
+import { TournamentsModalBg } from './assets/tournaments-modal-bg';
 import { TournamentsForm } from './TournamentsForm';
 
 interface TournamentsModalFormProps {
@@ -9,12 +9,12 @@ interface TournamentsModalFormProps {
 
 export function TournamentsModalForm({ onClose }: TournamentsModalFormProps) {
   return (
-    <div className="w-320 h-220 relative" onClick={(e) => e.stopPropagation()}>
-      <div className="h-full relative z-10 w-full">
+    <div className="w-320 h-200 relative" onClick={(e) => e.stopPropagation()}>
+      <div className="relative z-10 h-full w-full">
         <div className="h-full w-full px-4 py-4">
           <TournamentsForm onClose={onClose} />
         </div>
-        <MarketModalBg className="absolute inset-0 -z-10 size-full h-full w-full" />
+        <TournamentsModalBg className="absolute inset-0 -z-10 size-full h-full w-full" />
       </div>
     </div>
   );
