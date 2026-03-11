5b65417c1a297a7461f0c3fb213bbf87b130ab52
Ivan Kireev <31880548+sm4chnik@users.noreply.github.com>
2026-03-11 19:42:26 -0300

    add new base btns modes

---
+++ apps/frontend/src/components/shared/Button/assets/btn-lg-bg.tsx
@@ -0,0 +1,34 @@
+export const BtnLgBg = ({ className }: { className: string }) => {
+  return (
+    <svg
+      width="277"
+      height="64"
+      viewBox="0 0 277 64"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      preserveAspectRatio="none"
+    >
+      <path d="M267 0H10V5H267V0Z" fill="#070C19" />
+      <path d="M10 5H5V10H10V5Z" fill="#070C19" />
+      <path d="M15 5H10V10H15V5Z" fill="#D5D8DD" />
+      <path d="M30 5H15V10H30V5Z" fill="white" />
+      <path d="M257 5H30V10H257V5Z" fill="#D5D8DD" />
+      <path d="M262 5H257V10H262V5Z" fill="white" />
+      <path d="M267 5H262V10H267V5Z" fill="#D5D8DD" />
+      <path d="M272 5H267V10H272V5Z" fill="#070C19" />
+      <path d="M5 10H0V54H5V10Z" fill="#070C19" />
+      <path d="M10 10H5V54H10V10Z" fill="#747C8F" />
+      <path d="M15 10H10V15H15V10Z" fill="white" />
+      <path d="M267 10H15V54H267V10Z" fill="#ACB0BC" />
+      <path d="M272 10H267V54H272V10Z" fill="#747C8F" />
+      <path d="M277 10H272V54H277V10Z" fill="#070C19" />
+      <path d="M15 15H10V25H15V15Z" fill="#D5D8DD" />
+      <path d="M15 25H10V54H15V25Z" fill="#ACB0BC" />
+      <path d="M10 54H5V59H10V54Z" fill="#070C19" />
+      <path d="M267 54H10V59H267V54Z" fill="#747C8F" />
+      <path d="M272 54H267V59H272V54Z" fill="#070C19" />
+      <path d="M267 59H10V64H267V59Z" fill="#070C19" />
+    </svg>
+  );
+};
---
+++ apps/frontend/src/components/shared/Button/assets/btn-md-bg.tsx
@@ -0,0 +1,34 @@
+export const BtnMdBg = ({ className }: { className: string }) => {
+  return (
+    <svg
+      width="210"
+      height="64"
+      viewBox="0 0 210 64"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      preserveAspectRatio="none"
+    >
+      <path d="M200 0H10V5H200V0Z" fill="#070C19" />
+      <path d="M10 5H5V10H10V5Z" fill="#070C19" />
+      <path d="M15 5H10V10H15V5Z" fill="#D5D8DD" />
+      <path d="M30 5H15V10H30V5Z" fill="white" />
+      <path d="M190 5H30V10H190V5Z" fill="#D5D8DD" />
+      <path d="M195 5H190V10H195V5Z" fill="white" />
+      <path d="M200 5H195V10H200V5Z" fill="#D5D8DD" />
+      <path d="M205 5H200V10H205V5Z" fill="#070C19" />
+      <path d="M5 10H0V54H5V10Z" fill="#070C19" />
+      <path d="M10 10H5V54H10V10Z" fill="#747C8F" />
+      <path d="M15 10H10V15H15V10Z" fill="white" />
+      <path d="M200 10H15V54H200V10Z" fill="#ACB0BC" />
+      <path d="M205 10H200V54H205V10Z" fill="#747C8F" />
+      <path d="M210 10H205V54H210V10Z" fill="#070C19" />
+      <path d="M15 15H10V25H15V15Z" fill="#D5D8DD" />
+      <path d="M15 25H10V54H15V25Z" fill="#ACB0BC" />
+      <path d="M10 54H5V59H10V54Z" fill="#070C19" />
+      <path d="M200 54H10V59H200V54Z" fill="#747C8F" />
+      <path d="M205 54H200V59H205V54Z" fill="#070C19" />
+      <path d="M200 59H10V64H200V59Z" fill="#070C19" />
+    </svg>
+  );
+};
