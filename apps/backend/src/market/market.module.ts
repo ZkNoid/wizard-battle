6f49d93069bec512f6f8cfa0d4436612d496fdf8
aii23 <a.ivlev.om@gmail.com>
2026-05-01 21:46:43 +0300

    Removed indexer

--- apps/backend/src/market/market.module.ts
+++ apps/backend/src/market/market.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MarketOrder, MarketOrderSchema } from './schemas/market-order.schema';
-import { IndexerState, IndexerStateSchema } from './schemas/indexer-state.schema';
-import { MarketIndexerService } from './services/market-indexer.service';
 import { MarketService } from './services/market.service';
 import { MarketController } from './controllers/market.controller';
 
@@ -10,11 +8,10 @@ import { MarketController } from './controllers/market.controller';
   imports: [
     MongooseModule.forFeature([
       { name: MarketOrder.name, schema: MarketOrderSchema },
-      { name: IndexerState.name, schema: IndexerStateSchema },
     ]),
   ],
   controllers: [MarketController],
-  providers: [MarketIndexerService, MarketService],
-  exports: [MarketService, MarketIndexerService],
+  providers: [MarketService],
+  exports: [MarketService],
 })
 export class MarketModule {}
